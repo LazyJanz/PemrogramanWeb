4fbac1d569029e8c176e0ff371ec03b7710cbdfe
Muhamad Fauzan Ramdhani <muhamad.fauzan05f@gmail.com>
2024-03-28 18:18:33 +0800

    Modified Index and CSS

--- Tugas/UTS/index.php
+++ Tugas/UTS/index.php
@@ -12,58 +12,51 @@ require 'db_conn.php';
 </head>
 <body>
     <div class="main-section">
-       <div class="add-section">
-          <form action="app/add.php" method="POST" autocomplete="off">
-             <?php if(isset($_GET['mess']) && $_GET['mess'] == 'error'){ ?>
-                <input type="text" 
-                     name="title" 
-                     style="border-color: #ff6666"
-                     placeholder="This field is required" />
-              <button type="submit">Add &nbsp; <span>&#43;</span></button>
-
-             <?php }else{ ?>
-              <input type="text" 
-                     name="title" 
-                     placeholder="What do you need to do?" />
-              <button type="submit">Add &nbsp; <span>&#43;</span></button>
-             <?php } ?>
-          </form>
-       </div>
-       <?php 
+        <div class="header-container">
+            <h1 class="header-title">To-Do List</h1>
+            <p class="header-subtitle">Stay organized and get things done!</p>
+        </div>
+        <?php 
           $todos = $conn->query("SELECT * FROM todos ORDER BY id DESC");
-       ?>
-       <div class="show-todo-section">
-            <?php if($todos->rowCount() <= 0){ ?>
-                <div class="todo-item">
-                    <div class="empty">
-                        <img src="img/f.png" width="100%" />
-                        <img src="img/Ellipsis.gif" width="80px">
-                    </div>
-                </div>
-            <?php } ?>
-
-            <?php while($todo = $todos->fetch(PDO::FETCH_ASSOC)) { ?>
-                <div class="todo-item">
-                    <span id="<?php echo $todo['id']; ?>"
-                          class="remove-to-do">x</span>
-                    <?php if($todo['checked']){ ?> 
-                        <input type="checkbox"
-                               class="check-box"
-                               data-todo-id ="<?php echo $todo['id']; ?>"
-                               checked />
-                        <h2 class="checked"><?php echo $todo['title'] ?></h2>
-                    <?php }else { ?>
-                        <input type="checkbox"
-                               data-todo-id ="<?php echo $todo['id']; ?>"
-                               class="check-box" />
-                        <h2><?php echo $todo['title'] ?></h2>
+        ?>
+        <div class="add-show-section">
+            <div class="add-section">
+                <form action="app/add.php" method="POST" autocomplete="off">
+                    <?php if(isset($_GET['mess']) && $_GET['mess'] == 'error'){ ?>
+                        <input type="text" name="title" style="border-color: #ff6666" placeholder="This field is required" />
+                        <button type="submit">Add &nbsp; <span>&#43;</span></button>
+                    <?php }else{ ?>
+                        <input type="text" name="title" placeholder="Masukkan Item" />
+                        <button type="submit">Tambahkan &nbsp; <span>&#43;</span></button>
                     <?php } ?>
-                    <br>
-                    <small>created: <?php echo $todo['date_time'] ?></small> 
-                </div>
-            <?php } ?>
-       </div>
+                </form>
+            </div>
+
+            <div class="show-todo-section">
+                <?php if($todos->rowCount() <= 0){ ?>
+                    <div class="todo-item"></div>
+                <?php } ?>
+
+                <?php while($todo = $todos->fetch(PDO::FETCH_ASSOC)) { ?>
+                    <div class="todo-item">
+                        <span id="<?php echo $todo['id']; ?>" class="remove-to-do">x</span>
+                        <?php if($todo['checked']){ ?> 
+                            <input type="checkbox" class="check-box" data-todo-id ="<?php echo $todo['id']; ?>" checked />
+                            <h2 class="checked"><?php echo $todo['title'] ?></h2>
+                        <?php }else { ?>
+                            <input type="checkbox" data-todo-id ="<?php echo $todo['id']; ?>" class="check-box" />
+                            <h2><?php echo $todo['title'] ?>&nbsp;</h2>
+                        <?php } ?>
+                        <br>
+                        <small>created : <?php echo $todo['date_time'] ?></small> 
+                    </div>
+                <?php } ?>
+            </div>
+        </div>
     </div>
+
+    <script src="js/jquery-3.2.1.min.js"></script>
+
 
     <script src="js/jquery-3.2.1.min.js"></script>
 
